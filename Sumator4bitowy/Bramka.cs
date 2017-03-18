4a75e6a18880db9a8b5f05a15577a9c8efb070f4
Krzysztof Pietraszko <krzysztof@pietraszko.cba.pl>
2017-03-18 18:21:14 +0100

    Add project files.

---
+++ Sumator4bitowy/Bramka.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Sumator4bitowy
+{
+	delegate bool Dzialanie(bool a, bool b);
+	class Bramka
+	{
+		Wtyk[] Wejscia;
+		Wtyk _Wyjscie;
+		public Wtyk Wyjscie
+		{
+			get { return _Wyjscie; }
+		}
+		Dzialanie DzialanieBramki;
+		public Bramka(Dzialanie dzialanie)
+		{
+			DzialanieBramki = dzialanie;
+			_Wyjscie = new Wtyk();
+			Wejscia = new Wtyk[2];
+		}
+		public void UstawWtykiWejsciowe(params Wtyk[] wtykiWe)
+		{
+			Wejscia[0] = wtykiWe[0];
+			Wejscia[1] = wtykiWe[1];
+		}
+		public void PodlaczDoWejscia(int doceloweWejscie, Bramka poprzednia)
+		{
+			Wejscia[doceloweWejscie] = poprzednia.Wyjscie;
+		}
+		public void WykonajDzialanie()
+		{
+			_Wyjscie.Wartosc = DzialanieBramki(Wejscia[0].Wartosc, Wejscia[1].Wartosc);
+		}
+	}
+}
---
+++ Sumator4bitowy/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Sumator4bitowy
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		List<Bramka> Bramki = new List<Bramka>();
+		public MainWindow()
+		{
+			InitializeComponent();
+			string A = "1010"; string B = "1010";
+			Wtyk[] LiczbaA = new Wtyk[4];
+			Wtyk[] LiczbaB = new Wtyk[4];
+			for (int i = 0; i < 4; i++) //tworzenie wtykow bitow wejsciowych
+			{
+				bool bit;
+				bit = A[i] == '1';
+				LiczbaA[i] = new Wtyk(bit);
+				bit = B[i] == '1';
+				LiczbaB[i] = new Wtyk(bit);
+			}
+			//Przykład testowy do usuniecia
+			//Bramki.Add(new Bramka(OR));
+			//Bramki[0].UstawWtykiWejsciowe(false, true);
+			//Bramki.Add(new Bramka(OR));
+			//Bramki[1].UstawWtykiWejsciowe(false, true);
+
+			//Bramki.Add(new Bramka(AND));
+			//Bramki[2].PodlaczDoWejscia(0, Bramki[0]);
+			//Bramki[2].PodlaczDoWejscia(1, Bramki[1]);
+
+			Bramki.Add(new Bramka(XOR));
+			Bramki.Last().UstawWtykiWejsciowe(LiczbaA[3], LiczbaB[3]);
+			Bramki.Add(new Bramka(XOR));
+			Bramki.Last().UstawWtykiWejsciowe(new Wtyk(false), new Wtyk(false)); //pierwsze niewazne bo tam bedzie polaczenie, drugie to przeniesienie
+			Bramki.Last().PodlaczDoWejscia(0, Bramki[0]);
+
+			foreach (var bramka in Bramki)
+				bramka.WykonajDzialanie();
+			Console.WriteLine("Wyjscie ostatniej bramki: " + Bramki[Bramki.Count - 1].Wyjscie.Wartosc.ToString01());
+		}
+		bool AND(bool a, bool b)
+		{
+			Debug.WriteLine("Wykonuje {0} AND {1}= {2}",a.ToString01(), b.ToString01(), (a&b).ToString01());
+			return a & b;
+		}
+		bool OR(bool a, bool b)
+		{
+			Debug.WriteLine("Wykonuje {0} OR {1}= {2}", a.ToString01(), b.ToString01(), (a|b).ToString01());
+			return a | b;
+		}
+		bool XOR(bool a, bool b)
+		{
+			Debug.WriteLine("Wykonuje {0} XOR {1}= {2}", a.ToString01(), b.ToString01(), (a ^ b).ToString01());
+			return a ^ b;
+		}
+		
+	}
+}
