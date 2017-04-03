421f16cc0bd64100fe644674f9897db382245982
Krzysztof Pietraszko <krzysztof@pietraszko.cba.pl>
2017-04-03 17:58:15 +0200

    Dziala tylko za pierwszym razem

--- Sumator4bitowy/MainWindow.xaml.cs
+++ Sumator4bitowy/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,33 +23,32 @@ namespace Sumator4bitowy
 	public partial class MainWindow : Window
 	{
 		List<Bramka> Bramki = new List<Bramka>();
+		string LiczbaX = "1111";
+		string LiczbaY = "1111";
+		Wtyk[] X = new Wtyk[4];
+		Wtyk[] Y = new Wtyk[4];
+		Wtyk[] Wynik;
+		Wtyk Przeniesienie;
+
 		public MainWindow()
 		{
 			InitializeComponent();
-			string A = "1001"; string B = "1000";
-			Wtyk[] LiczbaA = new Wtyk[4];
-			Wtyk[] LiczbaB = new Wtyk[4];
-			Wtyk[] Wynik = new Wtyk[4];
-			Wtyk Przeniesienie = new Wtyk(false); //przeniesienie wejściowe pierwszego sumatora to 0
-			for (int i = 0; i < 4; i++) //tworzenie wtykow bitow wejsciowych i nadanie wartosci ze stringow
-			{
-				bool bit;
-				bit = A[i] == '1';
-				LiczbaA[i] = new Wtyk(bit);
-				bit = B[i] == '1';
-				LiczbaB[i] = new Wtyk(bit);
-			}
-			for (int i = 3; i >= 0; i--)
+			//string X = "1001"; string Y = "1000";
+			
+			Wynik = new Wtyk[4];
+			Przeniesienie = new Wtyk(false); //przeniesienie wejściowe pierwszego sumatora to 0
+			StworzWtykiWejsciowe();
+			for (int i = 3; i >= 0; i--) //łączenie bramek
 			{
 				Bramka BramkaXor = new Bramka(XOR);
 				Bramki.Add(BramkaXor);
-				BramkaXor.UstawWtykiWejsciowe(LiczbaA[i], LiczbaB[i]);
+				BramkaXor.UstawWtykiWejsciowe(X[i], Y[i]);
 				Bramki.Add(new Bramka(XOR));
 				Bramki.Last().UstawWtykiWejsciowe(BramkaXor.Wyjscie, Przeniesienie);
 				Wynik[i] = Bramki.Last().Wyjscie;
 				Bramka ANDGorny = new Bramka(AND);
 				Bramki.Add(ANDGorny);
-				ANDGorny.UstawWtykiWejsciowe(LiczbaA[i], LiczbaB[i]);
+				ANDGorny.UstawWtykiWejsciowe(X[i], Y[i]);
 				Bramka ANDDolny = new Bramka(AND);
 				Bramki.Add(ANDDolny);
 				ANDDolny.UstawWtykiWejsciowe(Przeniesienie, BramkaXor.Wyjscie);
@@ -57,23 +57,17 @@ namespace Sumator4bitowy
 				BramkaOR.UstawWtykiWejsciowe(ANDGorny.Wyjscie, ANDDolny.Wyjscie);
 				Przeniesienie = BramkaOR.Wyjscie;
 			}
-
-			foreach (var bramka in Bramki)
-				bramka.WykonajDzialanie();
-			Console.Write("Wynik: " + Przeniesienie.Wartosc.ToString01());
-			foreach (var wtyk in Wynik)
-				Console.Write(wtyk.Wartosc.ToString01());
-			Console.Write(Environment.NewLine);
-	
+			ObliczWynik();
 		}
+		#region operacje boolowskie
 		bool AND(bool a, bool b)
 		{
-			Debug.WriteLine("Wykonuje {0} AND {1}= {2}",a.ToString01(), b.ToString01(), (a&b).ToString01());
+			Debug.WriteLine("Wykonuje {0} AND {1}= {2}", a.ToString01(), b.ToString01(), (a & b).ToString01());
 			return a & b;
 		}
 		bool OR(bool a, bool b)
 		{
-			Debug.WriteLine("Wykonuje {0} OR {1}= {2}", a.ToString01(), b.ToString01(), (a|b).ToString01());
+			Debug.WriteLine("Wykonuje {0} OR {1}= {2}", a.ToString01(), b.ToString01(), (a | b).ToString01());
 			return a | b;
 		}
 		bool XOR(bool a, bool b)
@@ -81,6 +75,69 @@ namespace Sumator4bitowy
 			Debug.WriteLine("Wykonuje {0} XOR {1}= {2}", a.ToString01(), b.ToString01(), (a ^ b).ToString01());
 			return a ^ b;
 		}
-		
+		#endregion
+
+		void StworzWtykiWejsciowe()
+		{
+			for (int i = 0; i < 4; i++) //tworzenie wtykow bitow wejsciowych i nadanie wartosci ze stringow
+			{
+				Przeniesienie = new Wtyk(false);
+				bool bit;
+				bit = LiczbaX[i] == '1';
+				X[i] = new Wtyk(bit);
+				bit = LiczbaY[i] == '1';
+				Y[i] = new Wtyk(bit);
+			}
+		}
+		string ObliczWynik()
+		{
+
+			StworzWtykiWejsciowe();
+			foreach (var bramka in Bramki)
+				bramka.WykonajDzialanie();
+			string WynikNapis = "";
+			WynikNapis += Przeniesienie.Wartosc.ToString01();
+			Console.Write("Wynik: " + Przeniesienie.Wartosc.ToString01());
+			for (int i = 0; i < Wynik.Length; i++)
+			{
+				string bit = Wynik[i].Wartosc.ToString01();
+				Console.Write(bit);
+				WynikNapis += bit;
+			}
+			Console.Write(Environment.NewLine);
+			return WynikNapis;
+		}
+
+		private void liczba_TextChanged(object sender, TextChangedEventArgs e) //sprawdza poprawnosc przy kazdym znaku
+		{
+			TextBox liczba = sender as TextBox;
+			foreach (char c in liczba.Text)
+			{
+				if (c != '0' && c != '1')
+				{
+					MessageBox.Show("Tylko 0 lub 1!");
+					liczba.Text = liczba.Text.Replace(c.ToString(), "");
+				}
+			}
+		}
+
+		private void liczba_LostFocus(object sender, RoutedEventArgs e) //sprawdza poprawnosc po ukonczeniu wpisywania
+		{
+			TextBox liczba = sender as TextBox;
+			Regex liczbaBinarna = new Regex("[01]{4}");
+			if (liczbaBinarna.IsMatch(liczba.Text))
+			{
+				if (liczba.Name == "liczbaX")
+					LiczbaX = liczba.Text;
+				else
+					LiczbaY = liczba.Text;
+				PoleWyniku.Content = ObliczWynik();
+			}
+			else
+			{
+				MessageBox.Show("Błąd wprowadzania, wprowadź 4 cyfry binarne");
+				liczba.Text = "";
+			}
+		}
 	}
 }
--- Sumator4bitowy/MetodyRozszerzajace.cs
+++ Sumator4bitowy/MetodyRozszerzajace.cs
@@ -10,7 +10,7 @@ namespace Sumator4bitowy
 	{
 		public static string ToString01(this Boolean boolean)
 		{
-			return boolean == true ? "1" : "0";
+			return boolean ? "1" : "0";
 		}
 	}
 }
